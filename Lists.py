65ff897e66726fcd2c3161763d137f610c7ed457
DAVIDDO12 <168615558+DAVIDDO12@users.noreply.github.com>
2024-05-09 17:26:21 +1200

    Update Lists.py

--- Lists.py
+++ Lists.py
@@ -1,1 +1,9 @@
-
+numList= [7, 6, 23, 8.18, 18, 8, 7.2, 85, 915, 12]
+Biggestnumber = max(numList)
+Smallestnumber = min(numList)
+Sum = sum(numList)
+Numberofnumbers = len(numList)
+Average = Sum / Numberofnumbers
+print(Biggestnumber)
+print(Smallestnumber)
+print(Average)
