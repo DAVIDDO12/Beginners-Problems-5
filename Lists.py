32c701d2506a996623b0a8fcf266c44ea3fb2b8d
DAVIDDO12 <168615558+DAVIDDO12@users.noreply.github.com>
2024-05-12 17:09:51 +1200

    Update Lists.py

--- Lists.py
+++ Lists.py
@@ -11,3 +11,35 @@ index1=numList.index(Biggestnumber)
 index2=numList.index(Smallestnumber)
 print(index1)
 print(index2)
+
+stringsList = ["abc", "123", "2332", "aBBA", "heelloo", "1212", "DcEfD"]
+count = 0
+for stringitem in stringsList:
+    if len(stringitem) > 0 and stringitem[0] == stringitem[-1]:
+        count += 1
+    print(count)
+
+iLikePesto = []
+otherFoods = []
+for people in range(1,9):
+    favouritefood = input("What's your favourite food?")
+    if favouritefood.lower() == "pesto":
+        iLikePesto.append(favouritefood)
+    else:
+        otherFoods.append(favouritefood)
+print(len(iLikePesto))
+for num in range(len(iLikePesto)):
+    print("I like pesto")
+print("Other foods:")
+print(otherFoods)
+
+cerealList = []
+while True:
+    cereal = input("Enter cereal:")
+    if cereal.lower() in ["sultana and bran", "weetbix"]:
+        break
+    else:
+        cerealList.append(cereal)
+print("The cereals:")
+for cereal in cerealList:
+    print(cereal)
